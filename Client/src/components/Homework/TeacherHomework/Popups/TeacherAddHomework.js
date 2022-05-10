ade0df913a5d066230b5b18cf3c75a910740e0b1
moskalenkoDev <kolya-4444@ukr.net>
2022-05-11 00:39:47 +0300

    added calendar

--- Client/src/components/Homework/TeacherHomework/Popups/TeacherAddHomework.js
+++ Client/src/components/Homework/TeacherHomework/Popups/TeacherAddHomework.js
@@ -1,7 +1,14 @@
 import { useEffect, useRef, useState } from 'react';
+import { SingleDatePicker } from "react-dates";
+import "react-dates/lib/css/_datepicker.css";
+import "../../CalendarStyles.scss";
+import moment from 'moment';
+import 'moment/locale/ru';
+import 'moment/locale/uk'
+
 import * as ActionCreators from '../../../../Redux/Actions/actions_homework';
 
-export const TeacherAddHomework = ({ state, onHidePopup, homework_popup_active_type, school_subjects, current_class_id}) => {
+export const TeacherAddHomework = ({ state, onHidePopup, homework_popup_active_type, school_subjects, current_class_id }) => {
 
 
     let lang = state.lang.language;
@@ -9,7 +16,7 @@ export const TeacherAddHomework = ({ state, onHidePopup, homework_popup_active_t
     {
         ua: {
             popupHeader: "Додавання домашнього завдання",
-            dataTitle: "Оберіть дату",
+            dataTitle: "Дата",
             requiredField: "*Обов'язкове поле",
             subjectsBtnTitle: "Предмет",
             warningTitle: "Поля не мають бути пустими!",
@@ -21,7 +28,7 @@ export const TeacherAddHomework = ({ state, onHidePopup, homework_popup_active_t
         },
         ru: {
             popupHeader: "Добавление домашнего задания",
-            dataTitle: "Выберите дату",
+            dataTitle: "Дата",
             requiredField: "*Обязательное поле",
             subjectsBtnTitle: "Предмет",
             warningTitle: "Поля не должны быть пустыми!",
@@ -33,7 +40,7 @@ export const TeacherAddHomework = ({ state, onHidePopup, homework_popup_active_t
         },
         en: {
             popupHeader: "Adding homework",
-            dataTitle: "Choose data",
+            dataTitle: "Date",
             requiredField: "*Required field",
             subjectsBtnTitle: "Subject",
             warningTitle: "All fields must be filled!",
@@ -48,20 +55,22 @@ export const TeacherAddHomework = ({ state, onHidePopup, homework_popup_active_t
     const [is_active_drop_down, setIs_active_drop_down] = useState(false); // active_drop_down
     const [subjects_li_list, setSubjects_li_list] = useState([]);
     const [chosen_subject, setChosen_subject] = useState(null);
+    const [date, setDate] = useState(null);
+    const [focusedInput, setFocusedInput] = useState(null);
     const selected_li = useRef();
 
     const onChooseSubject = (e) => {
-        if(selected_li.current) selected_li.current.className = "";
+        if (selected_li.current) selected_li.current.className = "";
         e.target.className = "selected_li";
         setChosen_subject(e.target.innerText);
         setIs_active_drop_down("");
         selected_li.current = e.target;
     }
 
-    useEffect(()=> {
-        if(school_subjects) {
+    useEffect(() => {
+        if (school_subjects) {
             const our_li_list = school_subjects.map((subject, index) => {
-                return(<li key={index}>{subject}</li>)
+                return (<li key={index}>{subject}</li>)
             })
             setSubjects_li_list(our_li_list);
         }
@@ -69,13 +78,39 @@ export const TeacherAddHomework = ({ state, onHidePopup, homework_popup_active_t
             setIs_active_drop_down(false);
             setSubjects_li_list([]);
             setChosen_subject(null);
-            if(selected_li.current) {
+            if (selected_li.current) {
                 selected_li.current.className = "";
                 selected_li.current = null;
             }
         }
-    },[current_class_id]);
+    }, [current_class_id]);
 
+
+
+    const outsideRangeSelector = (day) => {
+        let diff = moment(day).diff(moment(), 'M');
+        if (diff < -2) return true;
+        return false;
+    }
+
+    const homeworkDays = ['10.05.2022', '06.05.2022', '01.05.2022'];
+
+    // const isDayHighlighted = date => {
+    //     let isHighlighted = false;
+    //     homeworkDays.forEach(hoveredDay => {
+    //         moment(hoveredDay);
+    //         const isDayOfMonthMatch = hoveredDay.date() === date.date();
+    //         const isMonthMatch = hoveredDay.month() === date.month();
+    //         const isYearMatch = hoveredDay.year() === date.year();
+    //         if (isDayOfMonthMatch && isMonthMatch && isYearMatch) {
+    //             isHighlighted = true;
+    //         }
+    //     });
+    //     return isHighlighted;
+    // };
+
+    moment.locale(lang === "ua" ? "uk" : lang);
+    // console.log(moment('10.05.2022', 'YYYY-MM-DD').toDate());
     return (
         <div className={"homework_popup add_homework_popup " + homework_popup_active_type}>
 
@@ -90,31 +125,49 @@ export const TeacherAddHomework = ({ state, onHidePopup, homework_popup_active_t
 
                     <div className="homework_popup_data_wrapper">
 
-                        {/* <div className="homework_choose_data"> */}
 
-                            <div className= {"subject_drop_down " + (is_active_drop_down ? "active_drop_down" : "")}>
+                        <div className={"subject_drop_down " + (is_active_drop_down ? "active_drop_down" : "")}>
 
-                                <button className='subject_drop_down_btn' onClick={() => setIs_active_drop_down(!is_active_drop_down)}>
-                                    <span className='subject_drop_down_header_selected'>{chosen_subject || langObj[lang].subjectsBtnTitle}</span>
-                                </button>
+                            <button className='subject_drop_down_btn' onClick={() => setIs_active_drop_down(!is_active_drop_down)}>
+                                <span className='subject_drop_down_header_selected'>{chosen_subject || langObj[lang].subjectsBtnTitle}</span>
+                            </button>
 
-                                <ul className= 'drop_down_li_list' onClick={(e) => onChooseSubject(e)}>
-                                    {subjects_li_list}
-                                </ul>
+                            <ul className='drop_down_li_list' onClick={(e) => onChooseSubject(e)}>
+                                {subjects_li_list}
+                            </ul>
 
-                            </div> 
+                        </div>
 
-                            {/* <span className='homework_choose_data_title'>{langObj[lang].subjectsBtnTitle}</span> */}
-                        {/* </div> */}
+                        <SingleDatePicker
+                            small
+                            date={date} // momentPropTypes.momentObj or null
+                            onDateChange={date => setDate(date)} // PropTypes.func.isRequired
+                            focused={focusedInput} // PropTypes.bool
+                            onFocusChange={({ focused }) => setFocusedInput(focused)} // PropTypes.func.isRequired
+                            id="add_homework_popup_date_picker" // PropTypes.string.isRequired,
+                            firstDayOfWeek={lang === "en" ? 0 : 1}
+                            showDefaultInputIcon
+                            numberOfMonths={1}
+                            daySize={30}
+                            anchorDirection="right"
+                            isOutsideRange={outsideRangeSelector}
+                            placeholder={langObj[lang].dataTitle}
+                            // isDayHighlighted={isDayHighlighted}
+                            // isDayHighlighted={(day) => {console.log(day); return true;}} 
+                            // monthFormat="YYYY[年]MMMM"
+                            // monthFormat = "MMMM YYYY"
+                            // enableOutsideDays 
+                            // showClearDate   
+                            // initialVisibleMonth={() => moment().subtract(2, 'months')}                          // start showing from adjusted month 
+                            // isDayBlocked = {(day) => true}                                                      // block all days, but only from availibles 
+                            hideKeyboardShortcutsPanel
 
-                        {/* <div className="homework_choose_data"> */}
-                            <input type="date" />
-                            {/* <span className='homework_choose_data_title'>{langObj[lang].dataTitle}</span> */}
-                        {/* </div> */}
+                        />
+
                     </div>
 
                     <div className="homework_popup_create_homework_message">
-                        <textarea placeholder= {langObj[lang].homeworkInputPlaceholder}></textarea>
+                        <textarea placeholder={langObj[lang].homeworkInputPlaceholder}></textarea>
                     </div>
 
                 </div>
--- Client/src/index.js
+++ Client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import {WrappedApp} from './components/App/App';
 import {Provider} from 'react-redux'; // провайдер нужен чтобы соединить редакс с реакт аппом
 import { store } from './Redux/store';
+import 'react-dates/initialize';
 
 if(!window.localStorage.getItem("language")) window.localStorage.setItem("language","ua");
 
